9b463abbf6ab9b947be00c3b700a11cb6ff8df0e
ZiluM <mzll1202@163.com>
2023-04-21 18:13:47 +0800

    0.0.18

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as fh:
+      long_des1 = fh.read()
+with open("CHANGELOG.md","r") as fh1:
+      long_des2 = fh1.read()
+long_des = long_des1 + "\n\n" + long_des2
+# print(long_des)
+
+
+print(find_packages())
+setup(name="sacpy",
+      version="0.0.18",
+      url="https://github.com/ZiluM/sacpy",
+      description="A repaid Statistical Analysis tool for Climate or Meteorology data.",
+      py_modules=["sacpy"],
+      include_package_data=True,
+      keywords="meteorology data statistic climate",
+      author="Zilu Meng",
+      author_email="mzll1202@163.com",
+      install_requires=["numpy", "scipy", "xarray"],
+      package_data={'sacpy': ['data/example/HadISST_sst_5x5.nc','data/example/NCEP_wind10m_5x5.nc']},
+      packages=find_packages(),
+      long_description=long_des,
+      long_description_content_type="text/markdown",
+      license="MIT")
+
+
+
